2cb2ffff1341318e972142a09e4d6af0b79c31db
rgomez <rgomez.ccna@gmail.com>
2025-11-05 16:22:08 -0300

    ajsutes de aistecia

--- resources/views/tablero/ministerio/form.blade.php
+++ resources/views/tablero/ministerio/form.blade.php
@@ -3,7 +3,7 @@
 
     <div class="d-flex justify-content-between align-items-center mb-3">
         <h5 class="mb-0 fw-bold text-secondary">
-            {{ isset($registro) ? 'Editar salida / Exhividor' : 'Nueva salida / Exhividor' }}
+            {{ isset($registro) ? 'Editar salida / Exhibidor' : 'Nueva salida / Exhibidor' }}
         </h5>
         
     </div>
